a51dd08df75eb2568c8201f2a2f8006fe2a63147
darkfirerdragon <75566048+darkfirerdragon@users.noreply.github.com>
2021-04-12 15:48:44 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+function setup() 
+{ canvas = createCanvas(300, 300); 
+  canvas.center(); video = createCapture(VIDEO); 
+  video.hide(); 
+  classifier = ml5.imageClassifier('MobileNet',modelLoaded); 
+} 
+  function modelLoaded() 
+  { console.log('Model Loaded!'); 
+} 
+function draw() 
+{ image(video, 0, 0, 300, 300); 
+  classifier.classify(video, gotResult); 
+} 
+function gotResult(error, results) 
+{ if (error) { 
+  console.log(error); 
+} 
+  else 
+  { console.log(results); 
+    document.getElementById("result").innerHTML = results[0].label; 
+    document.getElementById("Accuracy").innerHTML = results[0].confidence.toFixed(3); 
+  } 
+  }
